4df4f2bb7ecba1313f047973383fab23d24ba436
Zeynep Okyar <zynpokyar@gmail.com>
2021-05-10 00:54:25 +0300

    menuitem eklendi.

---
+++ src/main/java/restaurant/data/Menuitem.java
@@ -0,0 +1,47 @@
+package restaurant.data;
+
+public class Menuitem {
+    private int id;
+    private String name;
+    private double price;
+    private int typeid;
+
+    public Menuitem(int id, String name, double price, int typeid) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.typeid = typeid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getTypeid() {
+        return typeid;
+    }
+
+    public void setTypeid(int typeid) {
+        this.typeid = typeid;
+    }
+}
